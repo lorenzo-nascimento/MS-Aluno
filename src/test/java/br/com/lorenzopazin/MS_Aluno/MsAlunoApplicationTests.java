d250af7f4b1b4b2a084f2b463767f7d420e3b7e6
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-18 10:55:59 -0300

    Upload do projeto para o github

---
+++ src/test/java/br/com/lorenzopazin/MS_Aluno/MsAlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lorenzopazin.MS_Aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
